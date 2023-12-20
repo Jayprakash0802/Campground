1bc9093770f9e0b506b356485d8fc22216a33f3b
Jayprakash0802 <20bcs063@iiitdwd.ac.in>
2023-12-20 08:21:15 +0530

    Update clusterMap.js

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'cluster-map',
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-103.59179687498357, 40.66995747013945],
+    center: [20.5937, 78.9629],
     zoom: 3
 });
 
